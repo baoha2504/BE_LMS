4a471794dcf991e4e958e2397867b700c9f15c7d
Hama-anhttt <daoviethaanhttt@gmail.com>
2023-09-15 14:48:11 +0700

    push 15/9

---
+++ LMS_IMAGE/LMS_IMAGE/Controllers/VideoBaiGiangController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_IMAGE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoBaiGiangController : ControllerBase
+    {
+    }
+}
