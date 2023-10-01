ec46870c2b7dcaa9fd88c70c26ce2e5117bad8d9
Bao <89384723+baoha2504@users.noreply.github.com>
2023-10-01 12:03:49 +0700

    commit before merge

--- LMS_IMAGE/LMS_IMAGE/Entities/LMSContext.cs
+++ LMS_IMAGE/LMS_IMAGE/Entities/LMSContext.cs
@@ -57,7 +57,7 @@ namespace LMS_IMAGE.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=14.231.93.67,13393;Initial Catalog=LMS;Persist Security Info=True;User ID=sa;Password=123456");
+                optionsBuilder.UseSqlServer("Data Source=123.31.24.17,13393;Initial Catalog=LMS;Persist Security Info=True;User ID=sa;Password=123456");
             }
         }
 
